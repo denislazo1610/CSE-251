17cb77f32a787251844fba426367c5091bdfd281
Denis <denislazo1610@gmail.com>
2022-01-17 02:09:05 -0700

    Finish assignment week 2

--- cse251-course-master/week02/assignment/assignment.py
+++ cse251-course-master/week02/assignment/assignment.py
@@ -46,17 +46,45 @@ call_count = 0
 # TODO Add your threaded class definition here
 class request_thread(threading.Thread):
     def __init__(self, url):
-        print("Create thread class")
         threading.Thread.__init__(self)
         self.url = url
+        self.info = {}
         
     def run(self):
         info = requests.get(self.url)
-        print_dict(info.json())
-        # print("Running all function from threat")
+        self.info = info.json()
 
 
 # TODO Add any functions you need here
+
+def film(url):
+    print("GETTING ALL INFO FILM")
+    infoFilm = request_thread(url)
+    infoFilm.start()
+    infoFilm.join()
+    infoFilm = infoFilm.info["results"]
+    return infoFilm[5]
+
+def data(linkData):
+    threads = []
+    infoData = []
+    for url in linkData:
+        infoThread = request_thread(url)
+        infoThread.start()
+        threads.append(infoThread)
+    
+    for finish in threads:
+        finish.join()
+        infoData.append(finish.info["name"])
+
+    return infoData
+
+def palabra(palabras):
+    palabra = ''
+    for word in palabras:
+        palabra += word + ", "
+
+    return palabra
 
 
 def main():
@@ -64,15 +92,36 @@ def main():
     log.start_timer('Starting to retrieve data from swapi.dev')
 
     # TODO Retrieve Top API urls
-    informacion = request_thread(TOP_API_URL)
+    starWarsInfo = request_thread(TOP_API_URL)
 
-    informacion.start()
-    call_count =+ 1
-    informacion.join()
+    starWarsInfo.start()
+    starWarsInfo.join()
+
+    def organizing(info):
+        infoData = data(infoFilm[info])
+        log.write(f'{info.capitalize()}: {len(infoData)}')
+        infoData.sort()
+        log.write(palabra(infoData))
+        log.write('')
 
     # TODO Retireve Details on film 6
 
+    print("Link for FILMS")
+    infoFilm = film(starWarsInfo.info["films"])
+    log.write('----------------------------------------')
+    log.write(f'Title   : {infoFilm["title"]}')
+    log.write(f'Director: {infoFilm["director"]}')
+    log.write(f'Producer: {infoFilm["producer"]}')
+    log.write(f'Released: {infoFilm["release_date"]}')
+    log.write('')
+
+
     # TODO Display results
+    organizing("characters")
+    organizing("planets")
+    organizing("starships")
+    organizing("vehicles")
+    organizing("species")
 
     log.stop_timer('Total Time To complete')
     log.write(f'There were {call_count} calls to swapi server')
